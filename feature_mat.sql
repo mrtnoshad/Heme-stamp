8e02a9920c49773e6fbcc0b286f0c3743e65558c
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-01-13 23:12:21 -0800

    Update feature_mat.sql

--- feature_mat.sql
+++ feature_mat.sql
@@ -1,10 +1,13 @@
-SELECT demog.*,  DX.autoimmune_hist,	DX.leukemia_hist,	DX.blood_dis_hist
-      --LR.lab_abn_WBC_bool,	LR.lab_abn_HMG_bool,	LR.lab_abn_PLT_bool,	LR.lab_abn_NTR_bool,
-      --MH.PRED_med,	MH.METH_med,	MH.DEXA_med,	MH.RIT_med,	MH.HOR_med
+CREATE OR REPLACE TABLE Heme_stamp.feature_mat AS
+(
+SELECT demog.*,  DX.autoimmune_hist,	DX.leukemia_hist,	DX.blood_dis_hist,
+      LR.lab_abn_WBC_bool,	LR.lab_abn_HMG_bool,	LR.lab_abn_PLT_bool,	LR.lab_abn_NTR_bool,
+      MH.PRED_med,	MH.METH_med,	MH.DEXA_med,	MH.RIT_med,	MH.HOR_med
 FROM `Heme_stamp.demog` as demog
 LEFT JOIN
     `Heme_stamp.DX_history` as DX USING (ANON_ID)
---LEFT JOIN
---    `Heme_stamp.LAB_RES` as LR USING (ANON_ID)
---LEFT JOIN
---    `Heme_stamp.med_hist` as MH USING (ANON_ID)
+LEFT JOIN
+    `Heme_stamp.LAB_RES` as LR USING (ANON_ID)
+LEFT JOIN
+    `Heme_stamp.med_hist` as MH USING (ANON_ID)
+)    
