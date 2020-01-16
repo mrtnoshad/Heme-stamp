d44c869665b4a4e2208a047156f492c30881fda0
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-01-15 22:19:34 -0800

    Create all_abnorm_lab_feature_explor.sql

---
+++ all_abnorm_lab_feature_explor.sql
@@ -0,0 +1,18 @@
+WITH
+
+LR as
+(select rit_uid, group_lab_name, lab_name, result_flag, ord_num_value, reference_low, reference_high, result_time_jittered
+  from starr_datalake2018.lab_result
+)
+
+select LR.lab_name , count(*) as num
+  from Heme_stamp.Heme_stamp_data as HM 
+  Left join LR
+  ON LR.rit_uid = HM.ANON_ID
+  WHERE LR.result_flag is not null
+  AND HM.label = 1.0
+  AND CAST(LR.result_time_jittered as DATETIME) <  DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 10 DAY) 
+  AND CAST(LR.result_time_jittered as DATETIME) > DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 12 MONTH) 
+  GROUP BY LR.lab_name
+  ORDER BY num desc
+
