aae0ec398d6c27730958910c2da6b7a641c67c81
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-02-03 15:56:43 -0800

    Create med_hist_v2.sql

---
+++ med_hist_v2.sql
@@ -0,0 +1,23 @@
+
+CREATE OR REPLACE TABLE Heme_stamp.med_hist_v2 AS
+(
+
+WITH 
+
+Med_Hist as
+(select jc_uid, med_description, order_time_jittered
+  from starr_datalake2018.order_med
+)
+
+
+SELECT HM.ANON_ID, HM.label,
+  max(case when (Med_Hist.med_description like '%DEXAMETHASONE%') then 1 else 0 end) as DEXAMETHASONE_med_hist,
+  max(case when (Med_Hist.med_description like '%CHEMO%') then 1 else 0 end) as CHEMO_med_hist,
+  
+FROM Heme_stamp.Heme_stamp_data as HM 
+  Left join Med_Hist
+  ON Med_Hist.jc_uid = HM.ANON_ID
+  AND CAST(Med_Hist.order_time_jittered as DATETIME) <  DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 1 DAY) 
+  AND CAST(Med_Hist.order_time_jittered as DATETIME) > DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 18 MONTH) 
+  GROUP By HM.ANON_ID, HM.label
+)
