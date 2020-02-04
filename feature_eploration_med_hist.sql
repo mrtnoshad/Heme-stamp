df495a7517a881f9d69c4fec6c631b7a3a4e8209
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-02-03 23:02:28 -0800

    Update feature_eploration_med_hist.sql

--- feature_eploration_med_hist.sql
+++ feature_eploration_med_hist.sql
@@ -1,12 +1,31 @@
+
+--CREATE OR REPLACE TABLE Heme_stamp.med_hist_v2 AS
+
+
 WITH 
 
 Med_Hist as
 (select jc_uid, med_description, order_time_jittered
   from starr_datalake2018.order_med
-)
+),
 
-
-select Med_Hist.med_description , count(*)/50000 as perc  -- 51303/ 49727
+Label0 as
+(
+select Med_Hist.med_description , count(distinct Med_Hist.jc_uid) as perc  -- 51303/ 49727
+  from Heme_stamp.Heme_stamp_data as HM 
+  Left join Med_Hist
+  ON Med_Hist.jc_uid = HM.ANON_ID
+  -- WHERE LR.result_flag is not null
+  AND HM.label = 0.0 -- positive results
+  AND CAST(Med_Hist.order_time_jittered as DATETIME) <  DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 1 DAY) 
+  AND CAST(Med_Hist.order_time_jittered as DATETIME) > DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 18 MONTH) 
+  GROUP BY Med_Hist.med_description
+  ORDER BY perc desc
+),  
+  
+Label1 as
+(
+select Med_Hist.med_description , count(distinct Med_Hist.jc_uid) as perc  -- 51303/ 49727
   from Heme_stamp.Heme_stamp_data as HM 
   Left join Med_Hist
   ON Med_Hist.jc_uid = HM.ANON_ID
@@ -16,3 +35,11 @@ select Med_Hist.med_description , count(*)/50000 as perc  -- 51303/ 49727
   AND CAST(Med_Hist.order_time_jittered as DATETIME) > DATETIME_SUB( CAST(HM.date_collected_jit as DATETIME), INTERVAL 18 MONTH) 
   GROUP BY Med_Hist.med_description
   ORDER BY perc desc
+)
+
+
+ select Label0.med_description, (Label1.perc - Label0.perc) as diff
+ From Label0
+ Left Join Label1
+ Using (med_description)
+ Order by diff desc
